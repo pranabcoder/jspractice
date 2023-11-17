dd7977c4f9c852bcec8b32dc1595b855b0b372db
Pranab Ghosh <pranabit6@gmail.com>
2023-11-17 23:16:22 +0530

    added 02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all js code as new version
+
+// alert(3 + 3) - > we are using Node.js not browser
+// output : ReferenceError: alert is not defined
+
+// console.log(3 + 3); console.log("Pranas") - > not a good practice. Code readability is bad
+
