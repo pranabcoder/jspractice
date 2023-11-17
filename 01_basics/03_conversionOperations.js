68ebe7bddd8217db9464ab658eac7ab1c2d8741c
Pranab Ghosh <pranabit6@gmail.com>
2023-11-17 23:59:10 +0530

    added 02_dataTypes.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = 33;
+//let score = '33abc';
+let score = null;
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 0
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); // true
+console.log(typeof booleanIsLoggedIn); // boolean
+
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
