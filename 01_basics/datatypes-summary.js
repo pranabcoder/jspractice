d05a50c0c1dbeecf31efa5751a135009c2d22826
Pranab Ghosh <pranabit6@gmail.com>
2023-11-18 16:21:18 +0530

    added lines in datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive data types: number, string, boolean, null, undefined, Symbol, BigInt
+
+// Reference data types(Non primitive): object, array, function
+
+const score = 100;
+const scoreValue = 100.5;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+// console.log(id === anotherId);
+
+const bigNumber = 1234567890123456789012345678901234567890n;
+
+const heros = ["superman", "batman", "spider man"];
+
+let myObject ={
+    name: "superman",
+    age: 30
+}
+
+const myFunction = function (){
+    console.log("hello world");
+}
+// console.log(typeof myFunction); // function
+// console.log(typeof myObject); // object
+// console.log(typeof heros); // object
+// console.log(typeof bigNumber); // bigint
+// console.log(typeof score); // number
+// console.log(typeof isLoggedIn); // boolean
+// console.log(typeof outsideTemp); // object
+
