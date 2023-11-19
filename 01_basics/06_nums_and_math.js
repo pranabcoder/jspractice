67daa723c0660b1092ecc6abaadac0feb496f776
Pranab Ghosh <pranabit6@gmail.com>
2023-11-19 15:16:51 +0530

    added lines in 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -13,6 +13,23 @@ const balance = Number(1000);
 // const otherNum = 1123.8966;
 // console.log(otherNum.toPrecision(3)); // 1.12e+3
 const hundreds = 1000000;
-console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
-console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
-console.log(Number.MIN_VALUE); // 5e-324
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000
+// console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+// console.log(Number.MIN_VALUE); // 5e-324
+
+// +++++++++++++++++++++Math Object+++++++++++++++++++++
+console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.6)); // 5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.6)); // 4
+// console.log(Math.min(4, 5, 6, 7, 8, 9)); // 4
+// console.log(Math.max(4, 5, 6, 7, 8, 9)); // 9
+// console.log(Math.pow(2, 3)); // 8
+// console.log((Math.random() * 10) + 1); // 8.822043300353073
+// console.log(Math.floor(Math.random() * 10) + 1); // 8
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
