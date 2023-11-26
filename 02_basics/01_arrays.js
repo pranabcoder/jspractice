a6620fed2a7124b54354cb971104a9b2fced44cf
Pranab Ghosh <pranabit6@gmail.com>
2023-11-26 15:01:21 +0530

    added lines in 02_Basics/01_arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -10,7 +10,7 @@ const myArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 // Array methods -
 myArray.push(10); // add an element at the end of the array
-console.log(myArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(myArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // myArray.pop(); // remove the last element of the array
 // console.log(myArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 // myArray.unshift(9); // add an element at the beginning of the array
@@ -18,5 +18,8 @@ console.log(myArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // myArray.shift(); // remove the first element of the array
 // console.log(myArray); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(myArray.includes(9)); // true -< check if the array contains the value 9
-console.log(myArray.indexOf(3)); // 2 -< return the index of the value 3
+// console.log(myArray.indexOf(3)); // 2 -< return the index of the value 3
 
+const newArray = myArray.join()
+console.log(myArray);
+console.log(typeof newArray); // string
