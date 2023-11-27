708d6be1ee67135338fa43cb51e21c5ef6038e05
Pranab Ghosh <pranabit6@gmail.com>
2023-11-27 23:42:10 +0530

    added lines in 03_Basics/01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -25,4 +25,29 @@ function addTwoNumbers(num1, num2) {
 
 // addTwoNumbers(10, 20); // 30
 const result = addTwoNumbers(10, 20);
-console.log(`Result ${result}`); // undefined
+// console.log(`Result ${result}`); // 30
+
+// function loginUserMessage(username) {
+//     if(username === undefined){
+//         return "Please enter username";
+//     }
+//     return `${username} just logged in`;
+// }
+
+// function loginUserMessage(username) {
+//     if(!username){
+//         return "Please enter username";
+//     }
+//     return `${username} just logged in`;
+// }
+
+function loginUserMessage(username = 'Guest') {
+    if(!username){
+        return "Please enter username";
+    }
+    return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("John Miller")); // "John Miller just logged in"
+
+// console.log(loginUserMessage()); // " just logged in"
