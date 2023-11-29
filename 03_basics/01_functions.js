e65a50e1c3c6176f42f154072d52e8ee943b7396
Pranab Ghosh <pranabit6@gmail.com>
2023-11-29 21:46:15 +0530

    added lines in 03_Basics/01_functions.js and 02_scopes.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -48,6 +48,43 @@ function loginUserMessage(username = 'Guest') {
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessage("John Miller")); // "John Miller just logged in"
+// console.log(loginUserMessage("John Miller")); // "John Miller just logged in"
 
 // console.log(loginUserMessage()); // " just logged in"
+
+// function calculateCartPrice(...num1) {
+//     return num1;
+// }
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, )); // [200, 400, 500]
+// console.log(calculateCartPrice(200, 400, 500, 2000)); // [500, 2000]
+
+const user = {
+    username: 'John',
+    price: 199
+};
+
+function handleObject(anyobject){
+    console.log(`Username: ${anyobject.username} and Price: ${anyobject.price}`);
+}
+
+// handleObject(user); //  Username: John and Price: 199
+
+// handleObject({
+//    username: 'Sam',
+//    price: 299
+// });
+
+// const myNewArray = [10, 20, 30, 40, 50];
+
+function returnSecondElement(getArray){
+    return getArray[2];
+}
+
+// console.log(returnSecondElement(myNewArray)); // 20
+
+console.log(returnSecondElement([10, 20, 30, 40, 50])); // 30
