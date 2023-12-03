548ac077260def7b582bee2992a2bca6b14960c2
Pranab Ghosh <pranabit6@gmail.com>
2023-12-03 11:33:00 +0530

    added lines in 04_control_flow/one and switch.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,3 +21,47 @@ const isUserLoggedIn = true;
 // if (2 === '2'){
 //     console.log("executed")
 // } // not executed
+// const temperature = 41;
+// if (temperature < 50 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200;
+// if (score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`); // error
+
+// const balance = 1000;
+// if (balance > 500) console.log("Balance is greater than 500");
+
+// const balance = 1000;
+// if (balance > 500) console.log("Balance is greater than 500"), console.log("Balance is greater than 500"); // multiple statements but not recommended
+
+// const balance = 1000;
+
+// if (balance < 500) {
+//     console.log("Balance is less than 500");
+// } else if (balance < 750) {
+//     console.log("Balance is less than 750");
+// } else if (balance < 900) {
+//     console.log("Balance is less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEamil = true;
+//
+// if (userLoggedIn && debitCard && 2 === 2) {
+//     console.log("Allow to buy course");
+// }
+
+if (userLoggedIn || loggedInFromGoogle || loggedInFromEamil) {
+    console.log("Allow to buy course");
+}
