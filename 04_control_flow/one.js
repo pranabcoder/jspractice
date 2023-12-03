34b73355fb3a61fd2dfbb920ee605e3e8224976e
Pranab Ghosh <pranabit6@gmail.com>
2023-12-03 10:40:25 +0530

    added lines in 04_control_flow/one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,23 @@
+// // if
+//
+// if (true){
+//     // Will run if true
+// }
+//
+// if (false){
+//     // Will not run
+// }
+
+const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+//     console.log('User is logged in');
+// }
+// <(Less than), >(greater than), <=(Less than equal), >=(greater than equal ), ==(Equal to), !=(Not equal to)
+// === (Equal to and same type), !== (Not equal to and same type)
+// if (2 == '2'){
+//     console.log("executed")
+// } // executed
+
+// if (2 === '2'){
+//     console.log("executed")
+// } // not executed
