a3d6be9a0e6e3e87e6400392289c5643ecb2b0bd
Pranab Ghosh <pranabit6@gmail.com>
2023-12-14 22:59:13 +0530

    update switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,40 @@
+// const month = 4;
+const monthName = 'April';
+
+/*
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    default:
+        console.log('Month is not January, February, March or April');
+        break;
+}*/
+
+switch (monthName) {
+    case 'January':
+        console.log('Month is January');
+        break;
+    case 'February':
+        console.log('Month is February');
+        break;
+    case 'March':
+        console.log('Month is March');
+        break;
+    case 'April':
+        console.log('Month is April');
+        break;
+    default:
+        console.log('Month is not January, February, March or April');
+        break;
+}
+// switch case options are case-sensitive.
