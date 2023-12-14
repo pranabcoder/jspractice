9f356618769147d80343a5d7a4e1a5b51c947e28
Pranab Ghosh <pranabit6@gmail.com>
2023-12-14 23:20:44 +0530

    update switch.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,13 +1,27 @@
 const userEmail = "p@pranab.ai";
 
-if (userEmail) {
+/*if (userEmail) {
   console.log("Got user email");
 } else {
   console.log("Don't have user email");
-}
+}*/
 
 // false values
 // false, 0, "", null, undefined, NaN, 0n, -0, BigInt(0)
 
 // truthy values
 // true, 1, " ", "0", {}, [], -1, 0n, BigInt(-1), "false", function() {}
+
+// how to check array is empty or not
+/*
+const emptyArray= [];
+if (emptyArray.length === 0) {
+  console.log("Array is empty");
+}*/
+
+// How to check empty object
+/*const emptyObject = {};
+
+if (Object.keys(emptyObject).length === 0) {
+  console.log("Object is empty");
+}*/
