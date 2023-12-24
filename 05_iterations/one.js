59e7fe9197e664852a64c022f5db2f47b3142691
pranab <pranabit6@gmail.com>
2023-12-24 12:20:11 +0530

    05iterations/one.js updated

---
+++ 05_iterations/one.js
@@ -0,0 +1,13 @@
+// for loop example
+
+/*
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+*/
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
