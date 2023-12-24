bf2f7becae06b8d2041bcd9ced3df9b3bebd945b
pranab <pranabit6@gmail.com>
2023-12-24 14:16:13 +0530

    05iterations/two.js updated

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// while
+/*
+while (condition) {
+    
+}
+*/
+/*
+let index = 0;
+while (index < 10) {
+    console.log(`Value of index is: ${index}`);
+    index++;
+}
+*/
+/*
+let myArray = ['flash', 'batman', 'superman'];
+
+let index = 0;
+
+while (index < myArray.length) {
+    console.log(`Superhero values is: ${myArray[index]}`);
+    index++;
+}
+*/
+
+let score = 1;
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10);
