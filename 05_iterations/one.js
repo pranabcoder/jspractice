50beff2f99c9f6a74a8b7503d50100b2058eb823
pranab <pranabit6@gmail.com>
2023-12-24 13:33:34 +0530

    05iterations/one.js updated

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -7,7 +7,26 @@ for (let index = 0; index < array.length; index++) {
 }
 */
 
+/*
 for (let i = 0; i < 10; i++) {
     console.log(i);
+}
+*/
+/*
+for (let i = 0; i < 10; i++) {
+    
+    const element = i;
+    if (element === 5) {
+        console.log("5 is the best number");
+    }
+    
+    console.log(element);
+}
+*/
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner loop value: ${j} and outer loop value: ${i}`);
+    }
 }
 
