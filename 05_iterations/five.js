20e9693edb130635344cd1fe7a1a8b6553bb06eb
pranab <pranabit6@gmail.com>
2023-12-25 21:04:10 +0530

    updated five.js file

---
+++ 05_iterations/five.js
@@ -0,0 +1,18 @@
+// for each loop example
+
+const coding = ["js", "ruby", "py", "php", "java", "c++", "c#"];
+/*
+coding.forEach(function (item) {
+  console.log(item);
+}); */
+
+/*
+coding.forEach((item) => {
+    console.log(item);
+}); */
+
+function printMessage(item) {
+  console.log(item);
+}
+
+coding.forEach(printMessage);
