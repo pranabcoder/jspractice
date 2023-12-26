9e33cbc093b263af78f425127f6617a9a4b964fa
pranab <pranabit6@gmail.com>
2023-12-26 20:51:54 +0530

    seven.js updated

---
+++ 05_iterations/seven.js
@@ -0,0 +1,10 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNumbs = myNumbers.map((number) => {
+//     return number + 10;
+// });
+
+const newNumbs = myNumbers.map((number) => number + 10);
+console.log(newNumbs);
+
+
