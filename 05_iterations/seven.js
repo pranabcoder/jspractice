98171de90d92d88799b0262c8fa15f2be19e6b22
pranab <pranabit6@gmail.com>
2023-12-26 20:58:01 +0530

    seven.js updated

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,18 @@
+// map example
+
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // const newNumbs = myNumbers.map((number) => {
 //     return number + 10;
 // });
 
-const newNumbs = myNumbers.map((number) => number + 10);
+// const newNumbs = myNumbers.map((number) => number + 10);
+
+const newNumbs = myNumbers
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num >= 40);
+
 console.log(newNumbs);
 
 
