9c1c461b405c51ecd5d14d8a60a392c94e60d6b2
pranab <pranabit6@gmail.com>
2024-01-01 00:27:46 +0530

    07_projects/1-colorChangeer updated

--- 07_projects/1-colorChanger/chaiaurcode.js
+++ 07_projects/1-colorChanger/chaiaurcode.js
@@ -8,16 +8,19 @@ Array.from(buttons).forEach((button) => {
         console.log(e);
         console.log(e.target);
         if (e.target.id === 'grey') {
-            body.style.backgroundColor = 'grey';
+            body.style.backgroundColor = e.target.id;
         }
         if (e.target.id === 'white') {
-            body.style.backgroundColor = 'white';
+            body.style.backgroundColor = e.target.id;
         }
         if (e.target.id === 'blue') {
-            body.style.backgroundColor = 'blue';
+            body.style.backgroundColor = e.target.id;
         }
         if (e.target.id === 'yellow') {
-            body.style.backgroundColor = 'yellow';
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'purple') {
+            body.style.backgroundColor = e.target.id;
         }
     });
   });
