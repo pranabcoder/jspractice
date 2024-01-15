a802edea5fe9669c024ba103a857528fabce2434
Pranab Ghosh <pranabit6@gmail.com>
2024-01-15 23:17:59 +0530

    new project file added

---
+++ 07_projects/practice_project/1_array_practice.js
@@ -0,0 +1,21 @@
+/* user will provide an array of numbers - input = [1, 2, 3, 4]
+output = [[1,2,3,4],[3,5,7],[8,12],[20]]
+*/
+
+function transformArray(inputArray) {
+    let result = [inputArray];
+    while (inputArray.length > 1) {
+        let newArray = [];
+        for (let i = 0; i < inputArray.length - 1; i++) {
+            newArray.push(inputArray[i] + inputArray[i + 1]);
+        }
+        inputArray = newArray;
+        result.push(inputArray);
+    }
+    return result;
+}
+
+// Example usage
+let testInput = [1, 2, 3, 4];
+let transformedArray = transformArray(testInput);
+console.log(transformedArray);
