48561b422bc9a0cf2996bb80c7389649a321b20d
Pranab Ghosh <pranabit6@gmail.com>
2024-01-15 23:30:02 +0530

    1_array_practice using recursion

--- 07_projects/practice_project/1_array_practice.js
+++ 07_projects/practice_project/1_array_practice.js
@@ -2,6 +2,7 @@
 output = [[1,2,3,4],[3,5,7],[8,12],[20]]
 */
 
+/*
 function transformArray(inputArray) {
     let result = [inputArray];
     while (inputArray.length > 1) {
@@ -15,7 +16,25 @@ function transformArray(inputArray) {
     return result;
 }
 
-// Example usage
+
 let testInput = [1, 2, 3, 4];
 let transformedArray = transformArray(testInput);
 console.log(transformedArray);
+*/
+
+function transformArray(inputArray) {
+    if (inputArray.length <= 1) {
+        return [inputArray];
+    }
+
+    let newArray = [];
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        newArray.push(inputArray[i] + inputArray[i + 1]);
+    }
+    return [inputArray, ...transformArray(newArray)];
+}
+
+let testInput = [1, 2, 3, 4];
+let transformedArray = transformArray(testInput);
+console.log(transformedArray);
+
