4d580a91334060dd11d9deecfa9f16895d095cd8
Pranab Ghosh <pranabit6@gmail.com>
2024-01-26 20:34:49 +0530

    mathpi.js added

--- 10_classess_and_oop/getter_setter.js
+++ 10_classess_and_oop/getter_setter.js
@@ -3,8 +3,15 @@ class User {
         this.email = email;
         this.password = password;
     }
+    get password() {
+        // return this._password.toUpperCase();
+        return `${this._password} is your password`;
+    }
+    set password(password) {
+        this._password = password;
+    }
 }
 
-const userOne = new User('userone@gmail.com', '123456');
+const userOne = new User('userone@gmail.com', 'abc123456');
 
 console.log(userOne.password);
