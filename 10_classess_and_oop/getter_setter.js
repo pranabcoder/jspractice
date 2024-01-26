0a56117a6a81b3f655d0449f854abd923a5ad1f3
Pranab Ghosh <pranabit6@gmail.com>
2024-01-26 21:00:52 +0530

    Added getter and setter to the object

--- 10_classess_and_oop/getter_setter.js
+++ 10_classess_and_oop/getter_setter.js
@@ -3,6 +3,9 @@ class User {
         this.email = email;
         this.password = password;
     }
+    get email() {
+        return this._email.toUpperCase();
+    }
     get password() {
         // return this._password.toUpperCase();
         return `${this._password} is your password`;
@@ -10,8 +13,12 @@ class User {
     set password(password) {
         this._password = password;
     }
+    set email(email) {
+        this._email = email;
+    }
 }
 
 const userOne = new User('userone@gmail.com', 'abc123456');
 
 console.log(userOne.password);
+console.log(userOne.email);
