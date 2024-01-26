3c677b213afb6139d9780688463a8fa6d3684c75
Pranab Ghosh <pranabit6@gmail.com>
2024-01-27 00:37:38 +0530

    arrayAdvance.js added

--- 11_fun_with_js/arrayAdvance.js
+++ 11_fun_with_js/arrayAdvance.js
@@ -1,6 +1,6 @@
 const myArr = [];
 
-%DebugPrint(myArr);
+// %DebugPrint(myArr);
 
 // Two types are arrays in js - continuous, Holey
 
@@ -9,3 +9,32 @@ const myArr = [];
 // Packed elements
 
 // Double(float, string, function)
+
+const arrTwo = [1, 2, 3, 4, 5, 6];
+
+// PACKED_SMI_ELEMENTS
+
+arrTwo.push(8.0);
+
+// PACKED_DOUBLE_ELEMENTS
+
+arrTwo.push('7');
+
+// PACKED_ELEMENTS
+
+arrTwo[10] = 11;
+
+//HOLEY_ELEMENTS
+
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[9]);
+
+// Bound check 
+
+// hasOwnProperty(arrTwo, 9);
+// hasOwnProperty(arrTwo.prototype, 10);
+// hasOwnProperty(Object.prototype, 10);
+
+// holes are very expensive in js
+
