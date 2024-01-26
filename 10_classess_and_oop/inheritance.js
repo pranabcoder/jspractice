caa8cc69546c3198838571c562153a247cd34674
Pranab Ghosh <pranabit6@gmail.com>
2024-01-26 16:20:29 +0530

    10_classess_and_oop/inheritance.js

---
+++ 10_classess_and_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username);
+        this.email = email;
+        this.password = password;
+    }
+
+    addCourse(){
+        console.log(`A new course is added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher('chai', 'chai@email.com', '1234');
+chai.addCourse();
+chai.logMe();
+
+const masalachai = new User('masalachai');
+masalachai.logMe();
+
+console.log(chai === masalachai);
+console.log(chai instanceof Teacher);
+console.log(chai instanceof User);
